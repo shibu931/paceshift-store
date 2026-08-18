4cd78e7c644e0b40d93f7135f96d9f0e895fefd9
Shippu Kumar <Itzshippu@gmail.com>
2026-08-18 16:24:45 +0530

    knsdkg

--- components/layout/cart/CartSummary.tsx
+++ components/layout/cart/CartSummary.tsx
@@ -59,9 +59,7 @@ export function CartSummary({
           proceeding to checkout.
         </p>
       )}
-      <FastrrCheckoutButton
-        
-      />
+      <FastrrCheckoutButton items={items}/>
       {/* <Link
         href={
           hasUnavailableItems
