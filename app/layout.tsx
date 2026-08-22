2acb7e86007a98f75d8edc8646b4dbd68ade0512
Shippu Kumar <Itzshippu@gmail.com>
2026-08-22 14:20:10 +0530

    Added GTag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/common/Footer";
 import { Rajdhani, Work_Sans } from "next/font/google";
 import "./globals.css";
 import { CartDrawer } from "@/components/layout/cart/CartDrawer";
+import Script from "next/script";
 
 const rajdhani = Rajdhani({
   subsets: ["latin"],
@@ -27,7 +28,28 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${rajdhani.variable} ${workSans.variable}`}>
+      <head>
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M7Q3CTDK');`,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7Q3CTDK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         {children}
         <Footer />
